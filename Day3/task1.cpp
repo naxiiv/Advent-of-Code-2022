0d64f81c45321d357bcc954e5e71fd30d2952fcc
naxiiv <a.mielczarek@proton.me>
2022-12-06 14:24:46 +0200

    Day 3 done

--- Day3/task1.cpp
+++ Day3/task1.cpp
@@ -12,22 +12,31 @@ int grantIndex(char num) {
 
 int value(string rucksack) {
     int items[ALPHABET];
+    for(int i=0; i<ALPHABET; i++)
+        items[i] = 0;
+    
     for(int i = 0; i < rucksack.length(); i++) {
-        int index = grantIndex(rucksack.at(i));
-        cout << index << endl;
+        int index = grantIndex(rucksack.at(i)) - 1;
         if(i<rucksack.length()/2) {
-            return 1;
+            items[index] = 1;
         }
-    }    
-    return 2;
+        if(i>=(rucksack.length()/2)) {
+            if(items[index] == 1) {
+                cout << index + 1 << endl;
+                return index+1;
+            }
+        }
+    }   
+    return 0;
 }
 
 int main() {
     ifstream input ("input.txt");
     int ans = 0;
     string line;
-    while(input >> line)
+    while(input >> line) 
         ans += value(line);
+    
     cout << ans << endl;
     
     return 0;
